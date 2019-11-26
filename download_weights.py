b2bd634aba822b6c3ba3de7aeeffee850180c262
Filipe <filipe.ftec@gmail.com>
2019-11-26 16:45:36 -0300

    add deep pose estimation

--- download_weights.py
+++ download_weights.py
@@ -1,8 +1,12 @@
 import wget
 import os
 
-base_path = 'https://pjreddie.com/media/files/'
 
+
+open_pose_mpi_base = 'http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/mpi/'
+
+
+base_path = 'https://pjreddie.com/media/files/'
 base_git_config = 'https://raw.githubusercontent.com/pjreddie/darknet/master'
 base_path_config = '{}/cfg/'.format(base_git_config)
 base_data_config = '{}/data/'.format(base_git_config)
@@ -11,6 +15,7 @@ base_data_config = '{}/data/'.format(base_git_config)
 download_weight_files = ['yolov3.weights', 'yolov3-tiny.weights', 'yolov3-spp.weights']
 download_config_files = ['yolov3-spp.cfg','yolov3-tiny.cfg','yolov3.cfg']
 download_data_files = ['coco.names']
+open_pose_files = ['pose_deploy_linevec_faster_4_stages.prototxt','pose_iter_160000.caffemodel']
 
 dir_path = 'weights'
 dir_exists = os.path.exists(dir_path)
@@ -27,9 +32,11 @@ if not dir_exists:
     print ("Weight directory does not exist. Creating..")
     os.makedirs(dir_path)
 
-download_files(download_weight_files, dir_path,base_path)
+download_files(download_weight_files, dir_path, base_path)
 download_files(download_config_files, dir_path, base_path_config)
 download_files(download_data_files, dir_path, base_data_config)
+download_files(open_pose_files, dir_path, open_pose_mpi_base)
+
 
 
     
